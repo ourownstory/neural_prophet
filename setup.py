69c6ae9b4eb56dfcbf646b691dad335d614cdaca
ourownstory <ourownstory@users.noreply.github.com>
2020-10-12 22:47:07 -0700

    add readme to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ import setuptools
 with open('requirements.txt', 'r') as f:
     requirements = f.read().splitlines()
 
+# read the contents of README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    readme = f.read()
+
 setuptools.setup(
     name="neuralprophet",
     version="0.2.0",
@@ -14,4 +20,6 @@ setuptools.setup(
     install_requires=requirements,
     extras_require={"dev": [""], },
     setup_requires=["flake8"],
+    long_description=readme,
+    long_description_content_type="text/markdown",
 )
