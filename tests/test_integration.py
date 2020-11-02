ea4412fae5bf73f39deb1b78ca4e8f8318628421
ourownstory <ourownstory@users.noreply.github.com>
2020-11-01 21:40:26 -0800

    increase resolution weekly

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -131,7 +131,7 @@ class IntegrationTests(unittest.TestCase):
         log.info("testing: CUstom Seasonality")
         df = pd.read_csv(PEYTON_FILE)
         # m = NeuralProphet(n_lags=60, n_changepoints=10, n_forecasts=30, verbose=True)
-        other_seasons = False
+        other_seasons = True
         m = NeuralProphet(
             yearly_seasonality=other_seasons,
             weekly_seasonality=other_seasons,
