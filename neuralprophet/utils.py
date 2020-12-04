1648ce7374699cd43ca92f7b88e37b35413fa566
ourownstory <ourownstory@users.noreply.github.com>
2020-12-04 13:51:23 -0800

    add random seed helper

--- neuralprophet/utils.py
+++ neuralprophet/utils.py
@@ -466,3 +466,8 @@ class HiddenPrints:
     def __exit__(self, exc_type, exc_val, exc_tb):
         sys.stdout.close()
         sys.stdout = self._original_stdout
+
+
+def set_random_seed(seed=0):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
