12c43170142620943b7a9340cedb5cff6842e9ca
ourownstory <ourownstory@users.noreply.github.com>
2020-12-05 19:08:58 -0800

    fix train-speed issue and add tests

--- neuralprophet/forecaster.py
+++ neuralprophet/forecaster.py
@@ -439,13 +439,14 @@ class NeuralProphet:
                     self.country_holidays_config["country"], df["ds"]
                 )
         self.config_train.set_auto_batch_epoch(n_data=len(df))
-        self.config_train.apply_train_speed()
+        self.config_train.apply_train_speed(batch=True, epoch=True)
         dataset = self._create_dataset(df, predict_mode=False)  # needs to be called after set_auto_seasonalities
         loader = DataLoader(dataset, batch_size=self.config_train.batch_size, shuffle=True)
         if not self.fitted:
             self.model = self._init_model()  # needs to be called after set_auto_seasonalities
         if self.config_train.learning_rate is None:
             self.config_train.learning_rate = self._lr_range_test(dataset)
+        self.config_train.apply_train_speed(lr=True)
         self.optimizer = optim.AdamW(self.model.parameters())
         self.scheduler = optim.lr_scheduler.OneCycleLR(
             self.optimizer,
