37c4867988ec79cb7473b52f454992f242061992
ourownstory <ourownstory@users.noreply.github.com>
2021-03-24 16:09:05 -0700

    fix plot lagged reg bug

--- tests/debug.py
+++ tests/debug.py
@@ -120,7 +120,7 @@ def debug_one(verbose=True):
     test_integration.IntegrationTests.plot = plot
     itests = test_integration.IntegrationTests()
     ##
-    # itests.test_custom_changepoints()
+    # itests.test_lag_reg_deep()
 
     test_unit.UnitTests.plot = plot
     utests = test_unit.UnitTests()
