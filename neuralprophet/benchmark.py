502cf24f9ec1c19bac84e1e77286990de21ac346
ourownstory <ourownstory@users.noreply.github.com>
2021-10-19 00:16:38 +0200

    progress towards benchmark simpler

--- neuralprophet/benchmark.py
+++ neuralprophet/benchmark.py
@@ -1,12 +1,21 @@
 from dataclasses import dataclass, field
+from typing import List, Optional, Tuple, Type
+from abc import ABC, abstractmethod
+
 import pandas as pd
 import numpy as np
 from neuralprophet import NeuralProphet
 
 
-class NeuralProphetParams:
+@dataclass
+class Params:
+    params: dict
+
+
+class NeuralProphetParams(Params):
     def __init__(self, **kwargs):
         self.params = kwargs
+        super().__init__(self)
 
 
 NeuralProphetModel = NeuralProphet
@@ -39,23 +48,48 @@ class Model:
     >>>         model_class=NeuralProphet,
     >>>         params=params
     >>>     ))
+
     """
 
     model_name: str
-    model_class: object
+    model_class: Type
 
-    def init(self):
-        return self.model_class(**self.params)
+    def initialize(self, params):
+        return self.model_class(**params)
 
 
 @dataclass
 class NeuralProphetModel(Model):
-    model_name: "NeuralProphet"
-    model_class: NeuralProphet
+    data: Dataset
+    params: NeuralProphetParams
+    model_name: str = "NeuralProphet"
+    model_class: Type = NeuralProphet
+
+    def initialize(self, params):
+        return NeuralProphet(**params)
 
 
 @dataclass
-class SimpleExperiment:
+class Experiment(ABC):
+    model_class: Model
+    params: Params
+    data: Dataset
+    experiment_name: dict = field(init=False)
+
+    def __post_init__(self):
+        self.experiment_name = {
+            "data": self.data.name,
+            "model": self.model_class.model_name,
+            "params": str(self.params),
+        }
+
+    @abstractmethod
+    def fit(self):
+        pass
+
+
+@dataclass
+class SimpleExperiment(Experiment):
     """
     use example:
     >>> experiments = []
@@ -64,37 +98,28 @@ class SimpleExperiment:
     >>>         experiments.append(SimpleExperiment(model=m, dataset=d, test_percentage=0.2))
     """
 
-    model: object
-    dataset: Dataset
-    test_percentage: float
-    experiment_name: dict = field(init=False)
+    metrics: List[str]
+    test_percentage: int
 
-    def __post_init__(self):
-        self.experiment_name = {
-            "data": self.dataset.name,
-            "model": self.model.model_name,
-            "params": str(self.model.params),
-        }
-
-    def fit(self, metrics):
-        df_train, df_val = self.model.init().split_df(
-            df=self.dataset.df,
-            freq=self.dataset.freq,
+    def fit(self):
+        model = self.model_class(self.data, params=self.params)
+        df_train, df_val = model.split_df(
+            df=self.data.df,
+            freq=self.data.freq,
             valid_p=self.test_percentage,
         )
-        m = self.model.init()
-        metrics_train = m.fit(df=df_train, freq=self.dataset.freq)
-        metrics_val = m.test(df=df_val)
+        metrics_train = model.fit(df=df_train, freq=self.data.freq)
+        metrics_val = model.test(df=df_val)
         result_train = self.experiment_name.copy()
         result_val = self.experiment_name.copy()
-        for metric in metrics:
+        for metric in self.metrics:
             result_train[metric] = metrics_train[metric].values[-1]
             result_val[metric] = metrics_val[metric].values[-1]
         return result_train, result_val
 
 
 @dataclass
-class CrossValidationExperiment(SimpleExperiment):
+class CrossValidationExperiment(Experiment):
     """
     >>> experiments_cv = []
     >>> for d in datasets:
@@ -106,7 +131,7 @@ class CrossValidationExperiment(SimpleExperiment):
     num_folds: int
     fold_overlap_pct: float = 0
 
-    def fit(self, metrics):
+    def fit(self):
         folds = self.model.init().crossvalidation_split_df(
             df=self.dataset.df,
             freq=self.dataset.freq,
@@ -130,43 +155,43 @@ class CrossValidationExperiment(SimpleExperiment):
         return result_train, result_val
 
 
-@dataclass
-class SimpleExperiment:
-    """
-    use example:
-    >>> experiments = []
-    >>> for d in datasets:
-    >>>     for m in models:
-    >>>         experiments.append(SimpleExperiment(model=m, dataset=d, test_percentage=0.2))
-    """
-
-    model: Model
-    dataset: Dataset
-    test_percentage: float
-    experiment_name: dict = field(init=False)
-
-    def __post_init__(self):
-        self.experiment_name = {
-            "data": self.dataset.name,
-            "model": self.model.model_name,
-            "params": str(self.model.params),
-        }
-
-    def fit(self, metrics):
-        df_train, df_val = self.model.init().split_df(
-            df=self.dataset.df,
-            freq=self.dataset.freq,
-            valid_p=self.test_percentage,
-        )
-        m = self.model.init()
-        metrics_train = m.fit(df=df_train, freq=self.dataset.freq)
-        metrics_val = m.test(df=df_val)
-        result_train = self.experiment_name.copy()
-        result_val = self.experiment_name.copy()
-        for metric in metrics:
-            result_train[metric] = metrics_train[metric].values[-1]
-            result_val[metric] = metrics_val[metric].values[-1]
-        return result_train, result_val
+# @dataclass
+# class SimpleExperiment:
+#     """
+#     use example:
+#     >>> experiments = []
+#     >>> for d in datasets:
+#     >>>     for m in models:
+#     >>>         experiments.append(SimpleExperiment(model=m, dataset=d, test_percentage=0.2))
+#     """
+#
+#     model: Model
+#     dataset: Dataset
+#     test_percentage: float
+#     experiment_name: dict = field(init=False)
+#
+#     def __post_init__(self):
+#         self.experiment_name = {
+#             "data": self.dataset.name,
+#             "model": self.model.model_name,
+#             "params": str(self.model.params),
+#         }
+#
+#     def fit(self, metrics):
+#         df_train, df_val = self.model.init().split_df(
+#             df=self.dataset.df,
+#             freq=self.dataset.freq,
+#             valid_p=self.test_percentage,
+#         )
+#         m = self.model.init()
+#         metrics_train = m.fit(df=df_train, freq=self.dataset.freq)
+#         metrics_val = m.test(df=df_val)
+#         result_train = self.experiment_name.copy()
+#         result_val = self.experiment_name.copy()
+#         for metric in metrics:
+#             result_train[metric] = metrics_train[metric].values[-1]
+#             result_val[metric] = metrics_val[metric].values[-1]
+#         return result_train, result_val
 
 
 @dataclass
