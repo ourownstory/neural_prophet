46a8b1df2e358379af223fb50294e7388029196b
Oskar Triebe <ourownstory@users.noreply.github.com>
2021-11-30 12:43:01 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(dir_repo, "README.md"), encoding="utf-8") as f:
 setuptools.setup(
     name="neuralprophet",
     version="0.3.0",
-    description="A simple yet customizable forecaster",
+    description="Explainable Forecasting at Scale",
     author="Oskar Triebe",
     author_email="trieb@stanford.edu",
     url="https://github.com/ourownstory/neural_prophet",
