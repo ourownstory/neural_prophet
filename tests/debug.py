da0ae0cc690c536bc3e7bcfd0411419bfbbbf810
ourownstory <ourownstory@users.noreply.github.com>
2022-02-17 12:09:08 -0800

    shorten epochs, add test

--- tests/debug.py
+++ tests/debug.py
@@ -32,37 +32,7 @@ PLOT = False
 
 
 def test_auto_batch_epoch():
-    check = {
-        "1": (1, 200),
-        "10": (10, 200),
-        "100": (16, 160),
-        "1000": (32, 64),
-        "10000": (64, 25),
-        "100000": (128, 20),
-        "1000000": (256, 20),
-        "10000000": (512, 20),
-    }
-
-    observe = {}
-    # for n_data in [10, int(1e3), int(1e6)]:
-    for n_data, (batch_size, epochs) in check.items():
-        n_data = int(n_data)
-        c = configure.Train(
-            learning_rate=None,
-            epochs=None,
-            batch_size=None,
-            loss_func="mse",
-            ar_sparsity=None,
-            optimizer="SGD",
-        )
-        c.set_auto_batch_epoch(n_data=n_data)
-        observe["{}".format(n_data)] = (c.batch_size, c.epochs)
-        log.info("[config] n_data: {}, batch: {}, epoch: {}".format(n_data, c.batch_size, c.epochs))
-        log.info("[should] n_data: {}, batch: {}, epoch: {}".format(n_data, batch_size, epochs))
-        # assert c.batch_size == batch_size
-        # assert c.epochs == epochs
-    print(check)
-    print(observe)
+    pass
 
 
 if __name__ == "__main__":
--- tests/test_unit.py
+++ tests/test_unit.py
@@ -169,16 +169,20 @@ def test_add_lagged_regressors():
 
 def test_auto_batch_epoch():
     check = {
-        "1": (1, 500),
-        "10": (10, 500),
-        "100": (16, 320),
-        "1000": (32, 181),
-        "10000": (64, 102),
-        "100000": (128, 57),
-        "1000000": (256, 50),
-        "10000000": (256, 50),
+        "1": (1, 200),
+        "10": (10, 200),
+        "100": (16, 160),
+        "1000": (32, 64),
+        "10000": (64, 25),
+        "100000": (128, 20),
+        "1000000": (256, 20),
+        "10000000": (512, 20),
     }
-    for n_data in [10, int(1e3), int(1e6)]:
+
+    observe = {}
+    # for n_data in [10, int(1e3), int(1e6)]:
+    for n_data, (batch_size, epochs) in check.items():
+        n_data = int(n_data)
         c = configure.Train(
             learning_rate=None,
             epochs=None,
@@ -187,11 +191,12 @@ def test_auto_batch_epoch():
             ar_sparsity=None,
             optimizer="SGD",
         )
-        c.set_auto_batch_epoch(n_data)
-        log.debug("n_data: {}, batch: {}, epoch: {}".format(n_data, c.batch_size, c.epochs))
-        batch, epoch = check["{}".format(n_data)]
-        assert c.batch_size == batch
-        assert c.epochs == epoch
+        c.set_auto_batch_epoch(n_data=n_data)
+        observe["{}".format(n_data)] = (c.batch_size, c.epochs)
+        log.debug("[config] n_data: {}, batch: {}, epoch: {}".format(n_data, c.batch_size, c.epochs))
+        log.debug("[should] n_data: {}, batch: {}, epoch: {}".format(n_data, batch_size, epochs))
+        assert c.batch_size == batch_size
+        assert c.epochs == epochs
 
 
 def test_split_impute():
