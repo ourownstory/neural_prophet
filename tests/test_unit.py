8b01750d5e958cab7f1e0fa4cfdf34a617fb3d44
ourownstory <ourownstory@users.noreply.github.com>
2022-02-22 19:48:03 -0800

    add test for make-future

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -518,3 +518,40 @@ def test_newer_sample_weight():
     log.info("avg yhat last samples: {}".format(avg_y2))
     assert avg_y1 > -0.9
     assert avg_y2 > 0.1
+
+
+def test_make_future():
+    NROWS = 100
+    EPOCHS = 1
+    BATCH_SIZE = 100
+    df = pd.read_csv(PEYTON_FILE, nrows=NROWS)
+    df["A"] = df["y"].rolling(7, min_periods=1).mean()
+    df_future_regressor = pd.DataFrame({"A": np.arange(10)})
+
+    # without lags
+    m = NeuralProphet(
+        epochs=EPOCHS,
+        batch_size=BATCH_SIZE,
+    )
+    m = m.add_future_regressor(name="A")
+    metrics_df = m.fit(df)
+    future = m.make_future_dataframe(
+        df,
+        periods=10,
+        regressors_df=df_future_regressor,
+        n_historic_predictions=False,
+    )
+    assert len(future) == 10
+
+    # with lags
+    m = NeuralProphet(
+        n_lags=5,
+        n_forecasts=3,
+        epochs=EPOCHS,
+        batch_size=BATCH_SIZE,
+    )
+    m = m.add_future_regressor(name="A")
+    m = m.add_lagged_regressor(names="B")
+    metrics_df = m.fit(df)
+    future = m.make_future_dataframe(df, n_historic_predictions=10)
+    assert len(future) == 10 + 5 + 3
