7bd8d4872c5dfba478faabf5cc24bc87ce15479a
Oskar Triebe <ourownstory@users.noreply.github.com>
2022-02-28 22:41:44 -0800

    dummy commit to test CI

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ from typing import Dict, Any
 # -- Project information -----------------------------------------------------
 
 project = "NeuralProphet"
-copyright = "2021, Oskar Triebe"
+copyright = "2022, Oskar Triebe"
 author = "Oskar Triebe"
 
 
