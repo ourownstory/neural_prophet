2dde92821bb75b9ea17768febe86edf767e4c709
karl-richter <karl.richter@tum.de>
2022-06-12 20:01:41 +0200

    Replace slicing value assignment with frame.loc

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1302,7 +1302,7 @@ def test_drop_missing_values_after_imputation():
     )
     df = pd.read_csv(PEYTON_FILE, nrows=NROWS)
     # introduce large window of NaN values, from which samples will be dropped after imputation
-    df["y"][100:131] = np.nan
+    df.loc[100:131, "y"] = np.nan
     metrics = m.fit(df, freq="D", validation_df=None)
     future = m.make_future_dataframe(df, periods=60, n_historic_predictions=60)
     forecast = m.predict(df=future)
