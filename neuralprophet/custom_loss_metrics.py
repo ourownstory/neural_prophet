98e82aefee43270352f8456c611c686c817378e5
Kevin R. Chen <kevinrchen2@gmail.com>
2022-07-30 06:22:13 +0000

    Modified PinballLoss to multiply 2 for the median quantile and sum up the quantiles for each batch and no. forecast.

--- neuralprophet/custom_loss_metrics.py
+++ neuralprophet/custom_loss_metrics.py
@@ -32,8 +32,8 @@ class PinballLoss(_Loss):
         positive_losses = torch.tensor(self.quantiles).unsqueeze(dim=-1).unsqueeze(dim=0) * base_losses
         negative_losses = (1 - torch.tensor(self.quantiles).unsqueeze(dim=-1).unsqueeze(dim=0)) * base_losses
         pinball_losses = torch.where(differences >= 0, positive_losses, negative_losses)
-        # multiplier = torch.ones(size=(1, len(self.quantiles), 1))
-        # multiplier[:, 0, :] = 2
-        # pinball_losses = multiplier * pinball_losses  # double the loss for the median quantile
+        multiplier = torch.ones(size=(1, len(self.quantiles), 1))
+        multiplier[:, 0, :] = 2
+        pinball_losses = multiplier * pinball_losses  # double the loss for the median quantile
         # combined_loss = torch.mean(torch.sum(pinball_losses, dim=1))
         return pinball_losses
