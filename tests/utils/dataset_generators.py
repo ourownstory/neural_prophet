01234925211a784d521e7ae935ded623774d0ca2
Richard <richard@stromer.org>
2022-08-11 19:42:48 +0200

    add generate_event_dataset function to generate artifical event dataset

--- tests/utils/dataset_generators.py
+++ tests/utils/dataset_generators.py
@@ -15,3 +15,16 @@ def generate_holiday_dataset(country="US", years=[2022], y_default=1, y_holiday=
         df.loc[timestamps[0], "y"] = y_holiday
 
     return df
+
+
+def generate_event_dataset(events=["2022-01-01", "2022-01-10", "2022-01-31"], periods=31, y_default=1, y_event=1000):
+    """Generate dataset with regular y value and special y value for events."""
+    events.sort()
+
+    dates = pd.date_range(events[0], periods=periods, freq="D")
+    df = pd.DataFrame({"ds": dates, "y": y_default}, index=dates)
+
+    for event in events:
+        df.loc[event, "y"] = y_event
+
+    return df, events
