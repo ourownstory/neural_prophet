946c23684dd0b33c4aa14c7446e6709a89067e07
Richard Stromer <noxan@users.noreply.github.com>
2022-08-23 18:39:24 +0200

    Write test cases for events and holidays regularization (#707)
    * write test cases for regularization base function and holidays and events
    
    * add function to generate artificial holidays dataset
    
    * use generated dataset for holiday test cases
    
    * adjust test parameters and disable other neuralprophet components
    
    * adjust expected value range for event tests
    
    * add generate_event_dataset function to generate artifical event dataset
    
    * use artifical event dataset in event regularization test cases
    
    * use smaller batch size for event test cases as the dataset is much smaller too
    
    * remove unused variables and imports
    
    * remove unused variable in for loop
    
    * extend holiday test case with dataset override for y values and test for meaningless holidays
    
    * add meaningless event (with dataset generation override) to test case
    
    * use 10 as y value for all test case overrides
    
    * use 100 for holiday and event y value
    
    * remove test cases without regularization
    
    * change test case to ensure meaningful events have weights
    
    * reduce regularization to ensure meaningful events keep their weights
    
    * replace lessequal and moreequal with less and more
    
    * reduce regularization to 0.01 to be on the safe side with our tests

---
+++ tests/utils/dataset_generators.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+from neuralprophet.time_dataset import make_country_specific_holidays_df
+
+
+def generate_holiday_dataset(country="US", years=[2022], y_default=1, y_holiday=100, y_holidays_override={}):
+    """Generate dataset with special y values for country holidays."""
+
+    periods = len(years) * 365
+    dates = pd.date_range("%i-01-01" % (years[0]), periods=periods, freq="D")
+    df = pd.DataFrame({"ds": dates, "y": y_default}, index=dates)
+
+    holidays = make_country_specific_holidays_df(years, country)
+    for holiday_name, timestamps in holidays.items():
+        df.loc[timestamps[0], "y"] = y_holidays_override.get(holiday_name, y_holiday)
+
+    return df
+
+
+def generate_event_dataset(
+    events=["2022-01-01", "2022-01-10", "2022-01-13", "2022-01-31"],
+    periods=31,
+    y_default=1,
+    y_event=100,
+    y_events_override={},
+):
+    """Generate dataset with regular y value and special y value for events."""
+    events.sort()
+
+    dates = pd.date_range(events[0], periods=periods, freq="D")
+    df = pd.DataFrame({"ds": dates, "y": y_default}, index=dates)
+
+    for event in events:
+        df.loc[event, "y"] = y_events_override.get(event, y_event)
+
+    return df, events
