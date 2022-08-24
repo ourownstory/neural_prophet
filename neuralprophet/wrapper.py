12b4e0bf5ed0d1d5f55733fe574956cab62ac852
karl-richter <karl.richter@tum.de>
2022-08-24 12:13:53 +0200

    initial version of Prophet wrapper

--- neuralprophet/wrapper.py
+++ neuralprophet/wrapper.py
@@ -1,6 +1,33 @@
 from neuralprophet.forecaster import NeuralProphet
+import pandas as pd
 
 
 class Prophet(NeuralProphet):
     def __init__(self, *args, **kwargs):
+        # Set Prophet-like default args
+        kwargs["quantiles"] = [0.9, 0.1]
+        # Run the NeuralProphet function
         super(Prophet, self).__init__(*args, **kwargs)
+        # Overwrite NeuralProphet properties
+        self.name = "Prophet"
+        self.history = None
+
+    def fit(self, *args, **kwargs):
+        # Run the NeuralProphet function
+        metrics_df = super(Prophet, self).fit(*args, **kwargs)
+        # Store the df for future use like in Prophet
+        self.history = kwargs.get("df", args[0])
+        return metrics_df
+
+    def make_future_dataframe(self, *args, **kwargs):
+        # Set Prophet-like default args
+        kwargs["n_historic_predictions"] = kwargs.get("n_historic_predictions", True)
+        # Use the provided df or fallback to the stored df during fit()
+        try:
+            df = kwargs.get("df", args[0])
+        except:
+            df = self.history
+        kwargs["df"] = df
+        # Run the NeuralProphet function
+        df_future = super(Prophet, self).make_future_dataframe(**kwargs)
+        return df_future
