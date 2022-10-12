958dff3a47a97c34d7bfd84de40950364e3de513
Richard <richard@stromer.org>
2022-10-12 18:44:39 +0200

    use future typing annotations and remove typing string form

--- neuralprophet/configure.py
+++ neuralprophet/configure.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import inspect
 import logging
 import math
@@ -35,7 +37,7 @@ class Normalization:
     local_data_params: dict = None  # nested dict (key1: name of dataset, key2: name of variable)
     global_data_params: dict = None  # dict where keys are names of variables
 
-    def init_data_params(self, df, config_covariates: "Covar" = None, config_regressor=None, config_events=None):
+    def init_data_params(self, df, config_covariates: Covar = None, config_regressor=None, config_events=None):
         if len(df["ID"].unique()) == 1:
             if not self.global_normalization:
                 log.info("Setting normalization to global as only one dataframe provided for training.")
