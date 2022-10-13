30ab1ed7afe2795c218460548ddd88daa44100cd
karl-richter <karl.richter@tum.de>
2022-10-13 11:53:03 -0700

    added air passenger accuracy test

--- tests/test_accuracy.py
+++ tests/test_accuracy.py
@@ -33,8 +33,30 @@ def test_yosemite():
 
     # Accuracy
     accuracy_metrics = metrics.to_dict("records")[0]
+    log.info(accuracy_metrics)
     assert accuracy_metrics["MAE"] < 100.0
     assert accuracy_metrics["RMSE"] < 100.0
+    assert accuracy_metrics["Loss"] < 0.5
+
+    if PLOT:
+        m.plot(forecast)
+        m.plot_parameters()
+        plt.show()
+
+
+def test_air_passengers():
+    log.info("TEST Air Passengers")
+    df = pd.read_csv(AIR_FILE)
+    m = NeuralProphet(changepoints_range=0.95, n_changepoints=15, weekly_seasonality=False)
+    metrics = m.fit(df, freq="5min")
+    future = m.make_future_dataframe(df, periods=12 * 24, n_historic_predictions=48 * 24)
+    forecast = m.predict(future)
+
+    # Accuracy
+    accuracy_metrics = metrics.to_dict("records")[0]
+    log.info(accuracy_metrics)
+    assert accuracy_metrics["MAE"] < 500.0
+    assert accuracy_metrics["RMSE"] < 500.0
     assert accuracy_metrics["Loss"] < 0.5
 
     if PLOT:
