4d030314f5ea068d065106e3279e87baa1431d6f
Richard <richard@stromer.org>
2022-10-17 18:10:41 +0200

    ignore flake8 warnings on unused imports which are made accessible for public in __init__.py

--- neuralprophet/__init__.py
+++ neuralprophet/__init__.py
@@ -1,11 +1,11 @@
 # make version number accessible
-from ._version import __version__
+from ._version import __version__  # noqa: F401
 
 # make core features accessible
-from .forecaster import NeuralProphet
-from .utils import set_random_seed, set_log_level
-from .utils import save, load
-from .df_utils import split_df
+from .forecaster import NeuralProphet  # noqa: F401
+from .utils import set_random_seed, set_log_level  # noqa: F401
+from .utils import save, load  # noqa: F401
+from .df_utils import split_df  # noqa: F401
 
 
 import logging
