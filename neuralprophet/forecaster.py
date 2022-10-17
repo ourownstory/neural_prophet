e4a01f1507f8463c6854d2d523e43995d0a79462
Richard <richard@stromer.org>
2022-10-17 18:17:35 +0200

    ignore flake8 E722

--- neuralprophet/forecaster.py
+++ neuralprophet/forecaster.py
@@ -2383,7 +2383,7 @@ class NeuralProphet:
                     live_out.append("ExtremaPrinter")
                 live_loss = PlotLosses(outputs=live_out)
                 plot_live_loss = True
-            except:
+            except:  # noqa: E722
                 log.warning(
                     "To plot live loss, please install neuralprophet[live]."
                     "Using pip: 'pip install neuralprophet[live]'"
