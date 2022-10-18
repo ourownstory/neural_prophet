57fc00ebf82f1d41e52cfc9c3da0f7fc486493ae
karl-richter <karl.richter@tum.de>
2022-10-17 19:17:04 -0700

    updated cml config

--- tests/model_tests/train_benchmark_models.py
+++ tests/model_tests/train_benchmark_models.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import os
 import pathlib
 import pandas as pd
@@ -17,13 +15,11 @@ DATA_DIR = os.path.join(DIR, "test-data")
 PEYTON_FILE = os.path.join(DATA_DIR, "wp_log_peyton_manning.csv")
 AIR_FILE = os.path.join(DATA_DIR, "air_passengers.csv")
 YOS_FILE = os.path.join(DATA_DIR, "yosemite_temps.csv")
-PLOT = False
 
 # Important to set seed for reproducibility
 set_random_seed(42)
 
 
-#    log.info("TEST Peyton Manning")
 df = pd.read_csv(PEYTON_FILE)
 m = NeuralProphet()
 metrics = m.fit(df)
@@ -36,31 +32,25 @@ with open("tests/model_tests/PeytonManning.json", "w") as outfile:
     json.dump(accuracy_metrics, outfile)
 
 
-def test_yosemite():
-    log.info("TEST Yosemite Temps")
-    df = pd.read_csv(YOS_FILE)
-    m = NeuralProphet(
-        changepoints_range=0.95, n_changepoints=15, weekly_seasonality=False, epochs=50, learning_rate=0.02
-    )
-    metrics = m.fit(df, freq="5min")
-    future = m.make_future_dataframe(df, periods=int(len(df) * 0.1), n_historic_predictions=True)
-    forecast = m.predict(future)
+df = pd.read_csv(YOS_FILE)
+m = NeuralProphet(changepoints_range=0.95, n_changepoints=15, weekly_seasonality=False, epochs=50, learning_rate=0.02)
+metrics = m.fit(df, freq="5min")
+future = m.make_future_dataframe(df, periods=int(len(df) * 0.1), n_historic_predictions=True)
+forecast = m.predict(future)
 
-    # Accuracy
-    accuracy_metrics = metrics.to_dict("records")[0]
-    with open("tests/model_tests/YosemiteTemps.json", "w") as outfile:
-        json.dump(accuracy_metrics, outfile)
+# Accuracy
+accuracy_metrics = metrics.to_dict("records")[0]
+with open("tests/model_tests/YosemiteTemps.json", "w") as outfile:
+    json.dump(accuracy_metrics, outfile)
 
 
-def test_air_passengers():
-    log.info("TEST Air Passengers")
-    df = pd.read_csv(AIR_FILE)
-    m = NeuralProphet()
-    metrics = m.fit(df)
-    future = m.make_future_dataframe(df, periods=int(len(df) * 0.1), n_historic_predictions=True)
-    forecast = m.predict(future)
+df = pd.read_csv(AIR_FILE)
+m = NeuralProphet()
+metrics = m.fit(df)
+future = m.make_future_dataframe(df, periods=int(len(df) * 0.1), n_historic_predictions=True)
+forecast = m.predict(future)
 
-    # Accuracy
-    accuracy_metrics = metrics.to_dict("records")[0]
-    with open("tests/model_tests/AirPassengers.json", "w") as outfile:
-        json.dump(accuracy_metrics, outfile)
+# Accuracy
+accuracy_metrics = metrics.to_dict("records")[0]
+with open("tests/model_tests/AirPassengers.json", "w") as outfile:
+    json.dump(accuracy_metrics, outfile)
