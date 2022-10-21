b780b4277fbf3863153677aeb27915655ff4879f
Richard <richard@stromer.org>
2022-10-21 20:03:03 +0200

    run isort and black on files in the same loop

--- scripts/pre_commit.bash
+++ scripts/pre_commit.bash
@@ -5,17 +5,12 @@ set -e
 pyfiles=$(git diff --staged --name-only --diff-filter=d -- "*.py")
 for file in $pyfiles; do
   black "$file"
+  isort "$file"
   git add "$file"
 done
 
 notebooks=$(git diff --staged --name-only --diff-filter=d -- "*.ipynb")
 for file in $notebooks; do
   black "$file"
-  git add "$file"
-done
-
-pyfiles=$(git diff --staged --name-only --diff-filter=d -- "*.py")
-for file in $pyfiles; do
-  isort "$file"
   git add "$file"
 done
