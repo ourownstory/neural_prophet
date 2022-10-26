305a67e03d43138bb32bef59918b24a57473425a
Richard Stromer <noxan@users.noreply.github.com>
2022-10-26 18:55:22 +0200

    778 update docstrings to match typing of variables (#897)
    * define type for config future regressors
    
    * add typing to config_regressors
    
    * add typing for config_regressors in def tabularize_univariate_datetime
    
    * replace all OrderedDict docstrings for config_regressors with proper typing
    
    * rename all config_regressor to config_regressors
    
    * add docstring typings for configure.ConfigFutureRegressors
    
    * define ConfigEvents type
    
    * define type alias for configure.ConfigCountryHolidays
    
    * update all docstring types for configure.ConfigEvents
    
    * update all docstring types for configure.ConfigCountryHolidays
    
    * add optional typings for event and holiday configs

--- neuralprophet/time_net.py
+++ neuralprophet/time_net.py
@@ -56,7 +56,7 @@ class TimeNet(nn.Module):
         config_season=None,
         config_lagged_regressors: Optional[configure.ConfigLaggedRegressors] = None,
         config_regressors=None,
-        config_events=None,
+        config_events: Optional[configure.ConfigEvents] = None,
         config_holidays=None,
         n_forecasts=1,
         n_lags=0,
@@ -77,9 +77,9 @@ class TimeNet(nn.Module):
 
             config_lagged_regressors : configure.ConfigLaggedRegressors
                 Configurations for lagged regressors
-            config_regressors : OrderedDict
+            config_regressors : configure.ConfigFutureRegressors
                 Configs of regressors with mode and index.
-            config_events : OrderedDict
+            config_events : configure.ConfigEvents
 
             config_holidays : OrderedDict
 
