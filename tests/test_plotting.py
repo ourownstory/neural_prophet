91da74766cc4d5ed359913c7c75a94863cb5fd13
julioare94 <julio@arend.mobi>
2022-10-27 16:34:51 -0700

    Adjust tests

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -111,11 +111,11 @@ def test_plotly_parameters():
 def test_plotly_global_local_parameters():
     log.info("Global Modeling + Global Normalization")
     df = pd.read_csv(PEYTON_FILE, nrows=512)
-    df1_0 = df.iloc[:128, :].copy(deep=True)
+    df1_0 = df.copy(deep=True)
     df1_0["ID"] = "df1"
-    df2_0 = df.iloc[128:256, :].copy(deep=True)
+    df2_0 = df.copy(deep=True)
     df2_0["ID"] = "df2"
-    df3_0 = df.iloc[256:384, :].copy(deep=True)
+    df3_0 = df.copy(deep=True)
     df3_0["ID"] = "df3"
     m = NeuralProphet(
         n_forecasts=2,
