dd3122cfa1787f179167f0efbb116a064f16f76b
karl-richter <karl.richter@tum.de>
2022-11-03 18:29:50 -0700

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,4 @@
 import logging
 
-# matplotlib is known to spam logs, thus we set the logging level to warning 
+# matplotlib is known to spam logs, thus we set the logging level to warning
 logging.getLogger("matplotlib").setLevel(logging.WARNING)
