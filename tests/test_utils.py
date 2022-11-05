50519b1e0c364b7e38bd62743eff8146b6e185cc
Anastasiia <114727920+anastasiia-tovstolis@users.noreply.github.com>
2022-11-05 12:37:10 +0100

    Add unit tests for get/set params

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from collections import OrderedDict
 
 import pytest
 import os
@@ -6,8 +7,10 @@ import pathlib
 import pandas as pd
 import numpy as np
 import logging
-from neuralprophet import NeuralProphet
+from neuralprophet import NeuralProphet, configure, time_net
 from neuralprophet import save, load
+import torch
+import torch.nn as nn
 
 log = logging.getLogger("NP.test")
 log.setLevel("WARNING")
@@ -42,3 +45,151 @@ def test_save_load():
     m2 = load("test_save_model.np")
     future = m2.make_future_dataframe(df, periods=3)
     forecast = m2.predict(df=future)
+
+
+def test_get_set_simple_params():
+    m = NeuralProphet()
+    m.set_params(
+        name="new name",
+        n_forecasts=5,
+        n_lags=4,
+        max_lags=6,
+        fitted=True,
+        predict_steps=5,
+    )
+    a = m.get_params()
+    assert a["name"] == "new name"
+    assert a["n_forecasts"] == 5
+    assert a["n_lags"] == 4
+    assert a["max_lags"] == 6
+    assert a["fitted"]
+    assert a["predict_steps"] == 5
+
+
+def test_get_set_normalisation():
+    m = NeuralProphet()
+    norm = configure.Normalization(
+            normalize="auto",
+            global_normalization=True,
+            global_time_normalization=False,
+            unknown_data_normalization=False,
+        )
+    m.set_params(
+        config_normalization=norm,
+    )
+    a = m.get_params()
+    assert a["config_normalization"] == norm
+
+
+def test_get_set_missing():
+    m = NeuralProphet()
+    missing = configure.MissingDataHandling(False, 5, 5, True)
+    m.set_params(
+        config_missing=missing,
+    )
+    a = m.get_params()
+    assert a["config_missing"] == missing
+
+
+def test_get_set_metrics():
+    m = NeuralProphet()
+    metrics = None
+    m.set_params(
+        metrics=metrics
+    )
+    a = m.get_params()
+    assert a["metrics"] == metrics
+
+
+def test_get_set_ar():
+    m = NeuralProphet()
+    ar = configure.AR(5, 1.2)
+    m.set_params(
+        config_ar=ar
+    )
+    a = m.get_params()
+    assert a["config_ar"] == ar
+
+
+def test_get_set_trend():
+    m = NeuralProphet()
+    trend = configure.Trend("linear", [], 0, 0, 0, 0)
+    m.set_params(
+        config_trend= trend,
+    )
+    a = m.get_params()
+    assert a["config_trend"] == trend
+
+
+def test_get_set_season():
+    m = NeuralProphet()
+    season = configure.AllSeason(
+            mode="additive",
+            reg_lambda=3,
+            yearly_arg=True,
+            weekly_arg=False,
+            daily_arg=True,
+        )
+    m.set_params(
+        config_season= season
+    )
+    a = m.get_params()
+    assert a["config_season"] == season
+
+
+def test_get_empty_model_params():
+    m = NeuralProphet()
+    result = m.get_model_param()
+    assert result is None
+
+
+def test_get_set_new_model():
+    m = NeuralProphet()
+    m.set_model_param(
+            n_forecasts=2,
+            n_lags=3,
+            num_hidden_layers=0,
+            d_hidden=None
+        )
+    a = m.get_model_param()
+    assert a["n_forecasts"] == 2
+    assert a["n_lags"] == 3
+    assert a["num_hidden_layers"] == 0
+    assert a["d_hidden"] is None
+
+
+def test_get_set_model():
+    df = pd.read_csv(PEYTON_FILE, nrows=NROWS)
+    m = NeuralProphet(
+        epochs=EPOCHS,
+        batch_size=BATCH_SIZE,
+        learning_rate=LR,
+        n_lags=6,
+        n_forecasts=3,
+    )
+    _ = m.fit(df, freq="D")
+    m.set_model_param(
+            n_forecasts=2,
+            n_lags=3,
+            num_hidden_layers=0,
+            d_hidden=None
+        )
+    a = m.get_model_param()
+    assert a["n_forecasts"] == 2
+    assert a["n_lags"] == 3
+    assert a["num_hidden_layers"] == 0
+    assert a["d_hidden"] is None
+
+
+if __name__ == '__main__':
+    test_get_set_model()
+    test_get_set_season()
+    test_get_set_trend()
+    test_get_set_ar()
+    test_get_empty_model_params()
+    test_get_set_metrics()
+    test_get_set_missing()
+    test_get_set_new_model()
+    test_get_set_normalisation()
+    test_get_set_simple_params()
+    test_save_load()
