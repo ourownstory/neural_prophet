8e854742ae8af0d88d94bdc54312a5ee134b3096
Richard <richard@stromer.org>
2022-11-21 19:31:58 -0800

    rewrite main cli with proper argument parsing

--- neuralprophet/__main__.py
+++ neuralprophet/__main__.py
@@ -1,8 +1,13 @@
 """
 Invokes neuralprophet when module is run as a script.
 """
-
+import argparse
 from neuralprophet._version import __version__
 
+
+parser = argparse.ArgumentParser(description="NeuralProphet")
+parser.add_argument("-v", "--version", action="version", version="%(prog)s " + __version__)
+
+
 if __name__ == "__main__":
-    print(__version__)
+    parser.parse_args()
