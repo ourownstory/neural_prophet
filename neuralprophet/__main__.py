dfd4667f47bf5546ae495a83cc69e05b2b5bb4b6
Richard Stromer <noxan@users.noreply.github.com>
2022-11-30 15:43:06 -0800

    Rename -v to -V

--- neuralprophet/__main__.py
+++ neuralprophet/__main__.py
@@ -8,7 +8,7 @@ from neuralprophet._version import __version__
 
 def parse_args(args=None):
     parser = argparse.ArgumentParser(description="NeuralProphet")
-    parser.add_argument("-v", "--version", action="version", version="%(prog)s " + __version__)
+    parser.add_argument("-V", "--version", action="version", version="%(prog)s " + __version__)
     return parser.parse_args(args)
 
 
