a588a068caedd60edef3fae8154872a68c18a396
Richard <richard@stromer.org>
2022-12-01 22:12:27 -0800

    use season config directly instead of derived value

--- neuralprophet/time_net.py
+++ neuralprophet/time_net.py
@@ -1270,7 +1270,7 @@ class TimeNet(pl.LightningModule):
                 reg_loss += l_trend * reg_trend
 
             # Regularize seasonality: sparsify fourier term coefficients
-            l_season = self.config_train.reg_lambda_season
+            l_season = self.config_season.reg_lambda
             if self.season_dims is not None and l_season is not None and l_season > 0:
                 for name in self.season_params.keys():
                     reg_season = utils.reg_func_season(self.season_params[name])
