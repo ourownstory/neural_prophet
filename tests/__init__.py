d806dbed179a85ed0640da2addb969b671b55d84
Richard <richard@stromer.org>
2022-12-02 17:25:09 -0800

    hide spam from pil debug logs

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,3 +5,6 @@ logging.getLogger("matplotlib").setLevel(logging.WARNING)
 logging.getLogger("pytorch_lightning").setLevel(logging.INFO)
 logging.getLogger("torch").setLevel(logging.WARNING)
 logging.getLogger("fsspec").setLevel(logging.INFO)
+
+# prevent PIL from spaming the logs
+logging.getLogger("PIL").setLevel(logging.INFO)
