86e9a620eabf87a9451360d333c046489082552f
Richard <richard@stromer.org>
2022-12-14 14:17:50 -0800

    add typing for get_country_holidays

--- neuralprophet/hdays_utils.py
+++ neuralprophet/hdays_utils.py
@@ -1,9 +1,11 @@
+from typing import Iterable, Optional, Union
+
 import holidays as pyholidays
 
 from neuralprophet import hdays as hdays_part2
 
 
-def get_country_holidays(country, years):
+def get_country_holidays(country: str, years: Optional[Union[int, Iterable[int]]] = None):
     """Helper function to get holidays for a country."""
     try:
         holidays_country = getattr(hdays_part2, country)(years=years)
