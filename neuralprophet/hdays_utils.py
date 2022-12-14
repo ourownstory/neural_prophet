fc092f4043384fdef4b118667b2d3be3a6b8e176
Richard <richard@stromer.org>
2022-12-14 14:31:45 -0800

    extend docstring with parameters and return value

--- neuralprophet/hdays_utils.py
+++ neuralprophet/hdays_utils.py
@@ -6,7 +6,22 @@ from neuralprophet import hdays as hdays_part2
 
 
 def get_country_holidays(country: str, years: Optional[Union[int, Iterable[int]]] = None):
-    """Helper function to get holidays for a country."""
+    """
+    Helper function to get holidays for a country.
+
+    Parameters
+    ----------
+        country : str
+            Country name to retrieve country specific holidays
+        years : int, list
+            Year or list of years to retrieve holidays for
+
+    Returns
+    -------
+        set
+            All possible holiday dates and names of given country
+
+    """
     try:
         holidays_country = getattr(hdays_part2, country)(years=years)
     except AttributeError:
