8ad5088b523c7909a5889a9a7da4f0b74fc857f3
Kevin R. Chen <kevinrchen2@gmail.com>
2023-01-10 03:31:02 +0000

    Uncommented the tests in test_model_performance.py.

--- tests/test_model_performance.py
+++ tests/test_model_performance.py
@@ -113,7 +113,6 @@ def create_metrics_plot(metrics):
     return fig
 
 
-"""
 def test_PeytonManning():
     df = pd.read_csv(PEYTON_FILE)
     m = NeuralProphet()
@@ -178,4 +177,3 @@ def test_AirPassengers():
         json.dump(accuracy_metrics, outfile)
 
     create_metrics_plot(metrics).write_image(os.path.join(DIR, "tests", "metrics", "AirPassengers.svg"))
-"""
