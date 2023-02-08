26e173f04716a8e938d856b4a30954199593fa96
julioare94 <julio@arend.mobi>
2023-02-08 10:14:56 -0800

    Zwischen commit

--- neuralprophet/time_dataset.py
+++ neuralprophet/time_dataset.py
@@ -409,7 +409,7 @@ def tabularize_univariate_datetime(
             tabularized_input_shapes_str += f"    {key} {value.shape} \n"
     log.debug(f"Tabularized inputs shapes: \n{tabularized_input_shapes_str}")
 
-    return inputs, targets, config_missing.drop_missing
+    return inputs, targets, config_missing.drop_missing # inputs, targets, und auch df filtern und zurueckgeben. gibt wahrsicbelich errors though.
 
 
 def fourier_series(dates, period, series_order):
