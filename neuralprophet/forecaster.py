3ebc8c6b582ecda63275437e2bf86daa09d95531
julioare94 <julio@arend.mobi>
2023-03-08 22:43:23 -0800

    Adjusted non-lagged components for all possible cases
    * n_forecasts > prediction_frequency
    * n_forecasts < prediction_frequency
    * n_forecasts = prediction_frequency

--- neuralprophet/forecaster.py
+++ neuralprophet/forecaster.py
@@ -3313,13 +3313,11 @@ class NeuralProphet:
                             for date in dates_comp:
                                 d = pd.date_range(date, periods=self.n_forecasts + 1, freq=self.data_freq)
                                 ser = pd.concat((ser, pd.Series(d).iloc[1:]))
-                            df_comp = pd.DataFrame(
-                                {"ds": ser, "yhat": components[comp].flatten()}).drop_duplicates(
+                            df_comp = pd.DataFrame({"ds": ser, "yhat": components[comp].flatten()}).drop_duplicates(
                                 subset="ds"
                             )
                             df_comp, _ = df_utils.add_missing_dates_nan(df_comp, freq=self.data_freq)
-                            yhat = pd.merge(
-                                df_forecast.filter(["ds","ID"]), df_comp, on="ds", how="left")[
+                            yhat = pd.merge(df_forecast.filter(["ds","ID"]), df_comp, on="ds", how="left")[
                                 "yhat"
                             ].values
                     if j == 0:  # temporary condition to add only the median component
