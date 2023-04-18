b8399b4f1d2c0c25238b75e6df12586c4de94fff
Richard <richard@stromer.org>
2023-04-17 23:45:32 -0700

    remove all python 3.7 references (and update to 3.8)

--- setup.py
+++ setup.py
@@ -46,7 +46,6 @@ setuptools.setup(
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
