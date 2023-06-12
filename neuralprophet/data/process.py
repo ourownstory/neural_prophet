e143bdc35b24f8c4b1b9f7d1af93e30d254e381c
leoniewgnr <wagner.leonie@tum.de>
2023-06-12 14:54:53 -0700

    changed to j

--- neuralprophet/data/process.py
+++ neuralprophet/data/process.py
@@ -157,7 +157,7 @@ def _reshape_raw_predictions_to_forecst_df(
                     for date in dates_comp:
                         d = pd.date_range(date, periods=n_forecasts + 1, freq=freq)
                         ser = pd.concat((ser, pd.Series(d).iloc[1:]))
-                    df_comp = pd.DataFrame({"ds": ser, "yhat": components[comp][:, :, 0].flatten()}).drop_duplicates(
+                    df_comp = pd.DataFrame({"ds": ser, "yhat": components[comp][:, :, j].flatten()}).drop_duplicates(
                         subset="ds"
                     )
                     df_comp, _ = df_utils.add_missing_dates_nan(df=df_comp, freq=freq)
