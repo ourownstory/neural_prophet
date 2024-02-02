190e3b7c3147507b8e907c329a62e367e21015e4
ourownstory <ourownstory@users.noreply.github.com>
2024-02-01 18:26:40 -0800

    fix test_get_country_holidays

--- tests/test_event_utils.py
+++ tests/test_event_utils.py
@@ -7,12 +7,14 @@ from neuralprophet import event_utils
 
 
 def test_get_country_holidays():
-    assert issubclass(event_utils.get_country_holidays("TU").__class__, holidays.countries.turkey.TR) is True
+    # deprecated
+    # assert issubclass(event_utils.get_country_holidays("TU").__class__, holidays.countries.turkey.TR) is True
+    # new format
+    assert issubclass(event_utils.get_all_holidays(country=["TU", "US"], years=2025).__class__, dict) is True
 
     for country in ("UnitedStates", "US", "USA"):
-        us_holidays = event_utils.get_country_holidays(country, years=2019)
-        assert issubclass(us_holidays.__class__, holidays.countries.united_states.UnitedStates) is True
+        us_holidays = event_utils.get_all_holidays(country=country, years=[2019, 2020])
         assert len(us_holidays) == 10
 
-    with pytest.raises(AttributeError):
-        event_utils.get_country_holidays("NotSupportedCountry")
+    with pytest.raises(NotImplementedError):
+        event_utils.get_holiday_names("NotSupportedCountry")
