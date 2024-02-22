1f9b68701720ffefe27821935d2b6e672a9bf451
ourownstory <ourownstory@users.noreply.github.com>
2024-02-22 15:27:57 -0800

    remove multicore tests

--- scripts/pre_push.bash
+++ scripts/pre_push.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Running pre-push hook: pytest"
-if ! pytest tests -v --durations=0 --numprocesses=auto;
+if ! pytest tests -v --durations=0;
 then
  echo "Failed pytests. Pytests must pass before push!"
  exit 1
