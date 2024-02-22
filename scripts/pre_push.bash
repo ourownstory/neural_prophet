e19ff07c862aec1b282a88c5f17ab0c2c0c9cbc7
ourownstory <ourownstory@users.noreply.github.com>
2024-02-22 15:14:38 -0800

    speed up tests

--- scripts/pre_push.bash
+++ scripts/pre_push.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Running pre-push hook: pytest"
-if ! pytest tests -v --durations=0;
+if ! pytest tests -v --durations=0 -n auto;
 then
  echo "Failed pytests. Pytests must pass before push!"
  exit 1
