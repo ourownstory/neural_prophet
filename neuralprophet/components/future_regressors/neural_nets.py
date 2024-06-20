ccfadebfd3c3a82d077f6c0f68ebb95e05ce7f56
MaiBe-ctrl <maiisabensalah@gmail.com>
2024-06-20 16:28:23 -0700

    fixed tensor shapes in regressor

--- neuralprophet/components/future_regressors/neural_nets.py
+++ neuralprophet/components/future_regressors/neural_nets.py
@@ -34,7 +34,7 @@ class NeuralNetsFutureRegressors(FutureRegressors):
                     regressor_net.append(nn.Linear(d_inputs, self.d_hidden_regressors, bias=True))
                     d_inputs = self.d_hidden_regressors
                 # final layer has input size d_inputs and output size equal to no. of forecasts * no. of quantiles
-                regressor_net.append(nn.Linear(d_inputs, self.n_forecasts * len(self.quantiles), bias=False))
+                regressor_net.append(nn.Linear(d_inputs, len(self.quantiles), bias=False))
                 for lay in regressor_net:
                     nn.init.kaiming_normal_(lay.weight, mode="fan_in")
                 self.regressor_nets[regressor] = regressor_net
@@ -84,8 +84,6 @@ class NeuralNetsFutureRegressors(FutureRegressors):
                 x = nn.functional.relu(x)
             x = self.regressor_nets[name][i](x)
 
-        # segment the last dimension to match the quantiles
-        x = x.reshape(x.shape[0], self.n_forecasts, len(self.quantiles))
         return x
 
     def all_regressors(self, regressor_inputs, mode):
