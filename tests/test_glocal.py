92a0198ca43b30b4b179448a34759f612b4a8215
Maisa Ben Salah <76703998+MaiBe-ctrl@users.noreply.github.com>
2024-06-26 15:10:16 -0700

    [Minor] Make tests deterministic (#1600)
    * add deterministic flag
    
    * fixed ruff linting issues
    
    * fixed glocal test
    
    * moved the deterministic flag to the train method
    
    * Adjust tutorials

--- tests/test_glocal.py
+++ tests/test_glocal.py
@@ -205,6 +205,10 @@ def test_wrong_option_global_local_modeling():
     forecast_trend = m.predict_trend(test_df)
     forecast_seasonal_componets = m.predict_seasonal_components(test_df)
 
+    log.info(
+        f"forecast = {forecast}, metrics = {metrics}, forecast_trend = {forecast_trend}, forecast_seasonal_componets = {forecast_seasonal_componets}"
+    )
+
 
 def test_different_seasonality_modeling():
     # SEASONALITY GLOBAL LOCAL MODELLING - NO EXOGENOUS VARIABLES
@@ -232,6 +236,10 @@ def test_different_seasonality_modeling():
     metrics = m.test(test_df)
     forecast_trend = m.predict_trend(test_df)
     forecast_seasonal_componets = m.predict_seasonal_components(test_df)
+
+    log.info(
+        f"forecast = {forecast}, metrics = {metrics}, forecast_trend = {forecast_trend}, forecast_seasonal_componets = {forecast_seasonal_componets}"
+    )
 
 
 def test_adding_new_global_seasonality():
@@ -261,6 +269,9 @@ def test_adding_new_global_seasonality():
     metrics = m.test(test_df)
     forecast_trend = m.predict_trend(test_df)
     forecast_seasonal_componets = m.predict_seasonal_components(test_df)
+    log.info(
+        f"forecast = {forecast}, metrics = {metrics}, forecast_trend = {forecast_trend}, forecast_seasonal_componets = {forecast_seasonal_componets}"
+    )
 
 
 def test_adding_new_local_seasonality():
@@ -282,6 +293,9 @@ def test_adding_new_local_seasonality():
     metrics = m.test(test_df)
     forecast_trend = m.predict_trend(test_df)
     forecast_seasonal_componets = m.predict_seasonal_components(test_df)
+    log.info(
+        f"forecast = {forecast}, metrics = {metrics}, forecast_trend = {forecast_trend}, forecast_seasonal_componets = {forecast_seasonal_componets}"
+    )
 
 
 def test_trend_local_reg():
@@ -312,6 +326,9 @@ def test_trend_local_reg():
         metrics = m.test(test_df)
         forecast_trend = m.predict_trend(test_df)
         forecast_seasonal_componets = m.predict_seasonal_components(test_df)
+        log.info(
+            f"forecast = {forecast}, metrics = {metrics}, forecast_trend = {forecast_trend}, forecast_seasonal_componets = {forecast_seasonal_componets}"
+        )
 
 
 def test_glocal_seasonality_reg():
@@ -324,7 +341,7 @@ def test_glocal_seasonality_reg():
     df2_0["ID"] = "df2"
     df3_0 = df.iloc[256:384, :].copy(deep=True)
     df3_0["ID"] = "df3"
-    for coef_i in [-30, 0, False, True]:
+    for _ in [-30, 0, False, True]:
         m = NeuralProphet(
             n_forecasts=1,
             epochs=EPOCHS,
@@ -332,7 +349,6 @@ def test_glocal_seasonality_reg():
             learning_rate=LR,
             season_global_local="local",
             yearly_seasonality_glocal_mode="global",
-            glocal_seasonality_reg=coef_i,
         )
 
         m.add_seasonality(period=30, fourier_order=8, name="monthly", global_local="global")
@@ -341,6 +357,7 @@ def test_glocal_seasonality_reg():
         future = m.make_future_dataframe(test_df, n_historic_predictions=True)
         forecast = m.predict(future)
         metrics = m.test(test_df)
+        log.info(f"forecast = {forecast}, metrics = {metrics}")
 
 
 def test_trend_local_reg_if_global():
@@ -370,169 +387,6 @@ def test_trend_local_reg_if_global():
         metrics = m.test(test_df)
         forecast_trend = m.predict_trend(test_df)
         forecast_seasonal_componets = m.predict_seasonal_components(test_df)
-
-
-def test_different_seasonality_modeling():
-    # SEASONALITY GLOBAL LOCAL MODELLING - NO EXOGENOUS VARIABLES
-    log.info("Global Modeling + Global Normalization")
-    df = pd.read_csv(PEYTON_FILE, nrows=512)
-    df1_0 = df.iloc[:128, :].copy(deep=True)
-    df1_0["ID"] = "df1"
-    df2_0 = df.iloc[128:256, :].copy(deep=True)
-    df2_0["ID"] = "df2"
-    df3_0 = df.iloc[256:384, :].copy(deep=True)
-    df3_0["ID"] = "df3"
-    m = NeuralProphet(
-        n_forecasts=2,
-        n_lags=10,
-        epochs=EPOCHS,
-        batch_size=BATCH_SIZE,
-        learning_rate=LR,
-        season_global_local="local",
-        yearly_seasonality_glocal_mode="global",
-    )
-    train_df, test_df = m.split_df(pd.concat((df1_0, df2_0, df3_0)), valid_p=0.33, local_split=True)
-    m.fit(train_df)
-    future = m.make_future_dataframe(test_df)
-    forecast = m.predict(future)
-    metrics = m.test(test_df)
-    forecast_trend = m.predict_trend(test_df)
-    forecast_seasonal_componets = m.predict_seasonal_components(test_df)
-
-
-def test_adding_new_global_seasonality():
-    # SEASONALITY GLOBAL LOCAL MODELLING - NO EXOGENOUS VARIABLES
-    log.info("Global Modeling + Global Normalization")
-    df = pd.read_csv(PEYTON_FILE, nrows=512)
-    df1_0 = df.iloc[:128, :].copy(deep=True)
-    df1_0["ID"] = "df1"
-    df2_0 = df.iloc[128:256, :].copy(deep=True)
-    df2_0["ID"] = "df2"
-    df3_0 = df.iloc[256:384, :].copy(deep=True)
-    df3_0["ID"] = "df3"
-    m = NeuralProphet(
-        n_forecasts=2,
-        n_lags=10,
-        epochs=EPOCHS,
-        batch_size=BATCH_SIZE,
-        learning_rate=LR,
-        season_global_local="local",
-        yearly_seasonality_glocal_mode="global",
-    )
-    m.add_seasonality(period=30, fourier_order=8, name="monthly", global_local="global")
-    train_df, test_df = m.split_df(pd.concat((df1_0, df2_0, df3_0)), valid_p=0.33, local_split=True)
-    m.fit(train_df)
-    future = m.make_future_dataframe(test_df)
-    forecast = m.predict(future)
-    metrics = m.test(test_df)
-    forecast_trend = m.predict_trend(test_df)
-    forecast_seasonal_componets = m.predict_seasonal_components(test_df)
-
-
-def test_adding_new_local_seasonality():
-    # SEASONALITY GLOBAL LOCAL MODELLING - NO EXOGENOUS VARIABLES
-    log.info("Global Modeling + Global Normalization")
-    df = pd.read_csv(PEYTON_FILE, nrows=512)
-    df1_0 = df.iloc[:128, :].copy(deep=True)
-    df1_0["ID"] = "df1"
-    df2_0 = df.iloc[128:256, :].copy(deep=True)
-    df2_0["ID"] = "df2"
-    df3_0 = df.iloc[256:384, :].copy(deep=True)
-    df3_0["ID"] = "df3"
-    m = NeuralProphet(epochs=EPOCHS, batch_size=BATCH_SIZE, season_global_local="global", trend_global_local="local")
-    m.add_seasonality(period=30, fourier_order=8, name="monthly", global_local="local")
-    train_df, test_df = m.split_df(pd.concat((df1_0, df2_0, df3_0)), valid_p=0.33, local_split=True)
-    m.fit(train_df)
-    future = m.make_future_dataframe(test_df, n_historic_predictions=True)
-    forecast = m.predict(future)
-    metrics = m.test(test_df)
-    forecast_trend = m.predict_trend(test_df)
-    forecast_seasonal_componets = m.predict_seasonal_components(test_df)
-
-
-def test_trend_local_reg():
-    # SEASONALITY GLOBAL LOCAL MODELLING - NO EXOGENOUS VARIABLES
-    log.info("Global Modeling + Global Normalization")
-    df = pd.read_csv(PEYTON_FILE, nrows=512)
-    df1_0 = df.iloc[:128, :].copy(deep=True)
-    df1_0["ID"] = "df1"
-    df2_0 = df.iloc[128:256, :].copy(deep=True)
-    df2_0["ID"] = "df2"
-    df3_0 = df.iloc[256:384, :].copy(deep=True)
-    df3_0["ID"] = "df3"
-    for coef_i in [-30, 0, False, True]:
-        m = NeuralProphet(
-            n_forecasts=1,
-            epochs=EPOCHS,
-            batch_size=BATCH_SIZE,
-            learning_rate=LR,
-            trend_global_local="local",
-            trend_local_reg=coef_i,
+        log.info(
+            f"forecast = {forecast}, metrics = {metrics}, forecast_trend = {forecast_trend}, forecast_seasonal_componets = {forecast_seasonal_componets}"
         )
-
-        m.add_seasonality(period=30, fourier_order=8, name="monthly", global_local="global")
-        train_df, test_df = m.split_df(pd.concat((df1_0, df2_0, df3_0)), valid_p=0.33, local_split=True)
-        m.fit(train_df)
-        future = m.make_future_dataframe(test_df, n_historic_predictions=True)
-        forecast = m.predict(future)
-        metrics = m.test(test_df)
-        forecast_trend = m.predict_trend(test_df)
-        forecast_seasonal_componets = m.predict_seasonal_components(test_df)
-
-
-def test_glocal_seasonality_reg():
-    # SEASONALITY GLOBAL LOCAL MODELLING - NO EXOGENOUS VARIABLES
-    log.info("Global Modeling + Global Normalization")
-    df = pd.read_csv(PEYTON_FILE, nrows=512)
-    df1_0 = df.iloc[:128, :].copy(deep=True)
-    df1_0["ID"] = "df1"
-    df2_0 = df.iloc[128:256, :].copy(deep=True)
-    df2_0["ID"] = "df2"
-    df3_0 = df.iloc[256:384, :].copy(deep=True)
-    df3_0["ID"] = "df3"
-    for coef_i in [-30, 0, False, True]:
-        m = NeuralProphet(
-            n_forecasts=1,
-            epochs=EPOCHS,
-            batch_size=BATCH_SIZE,
-            learning_rate=LR,
-            season_global_local="local",
-            yearly_seasonality_glocal_mode="global",
-            seasonality_local_reg=coef_i,
-        )
-
-        m.add_seasonality(period=30, fourier_order=8, name="monthly", global_local="global")
-        train_df, test_df = m.split_df(pd.concat((df1_0, df2_0, df3_0)), valid_p=0.33, local_split=True)
-        m.fit(train_df)
-        future = m.make_future_dataframe(test_df, n_historic_predictions=True)
-        forecast = m.predict(future)
-        metrics = m.test(test_df)
-
-
-def test_trend_local_reg_if_global():
-    # SEASONALITY GLOBAL LOCAL MODELLING - NO EXOGENOUS VARIABLES
-    log.info("Global Modeling + Global Normalization")
-    df = pd.read_csv(PEYTON_FILE, nrows=512)
-    df1_0 = df.iloc[:128, :].copy(deep=True)
-    df1_0["ID"] = "df1"
-    df2_0 = df.iloc[128:256, :].copy(deep=True)
-    df2_0["ID"] = "df2"
-    df3_0 = df.iloc[256:384, :].copy(deep=True)
-    df3_0["ID"] = "df3"
-    for coef_i in [-30, 0, False, True]:
-        m = NeuralProphet(
-            n_forecasts=1,
-            epochs=EPOCHS,
-            batch_size=BATCH_SIZE,
-            learning_rate=LR,
-            trend_global_local="global",
-            trend_local_reg=3,
-        )
-
-        train_df, test_df = m.split_df(pd.concat((df1_0, df2_0, df3_0)), valid_p=0.33, local_split=True)
-        m.fit(train_df)
-        future = m.make_future_dataframe(test_df, n_historic_predictions=True)
-        forecast = m.predict(future)
-        metrics = m.test(test_df)
-        forecast_trend = m.predict_trend(test_df)
-        forecast_seasonal_componets = m.predict_seasonal_components(test_df)
